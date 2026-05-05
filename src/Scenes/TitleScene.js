7a81138efab8ca765fb2cedf33f6ea645a88021f
smoeyyy <trwillis@ucsc.edu>
2026-05-05 12:26:14 -0700

    title

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -11,7 +11,7 @@ class TitleScene extends Phaser.Scene{
     create() {
         document.getElementById('description').innerHTML = '<h2>Red Fish Blue Fish - Trinity Willis </h2>'
 
-        this.add.text(game.config.width/2, game.config.height/2, "CLICK TO START\n\nESC for Controls", {
+        this.add.text(game.config.width/2, game.config.height/2, "Red Fish Blue Fish\n\nCLICK TO START\n\nESC for Controls", {
             fontSize: "32px",
         }).setOrigin(0.5);
 
